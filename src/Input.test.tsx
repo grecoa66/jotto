78871f25d3756b0b1a21f4ec7c66d3295141b851
Alex Greco <alex.k.greco.4@gmail.com>
2021-06-03 13:41:38 -0400

    - Complete testing challenge

--- src/Input.test.tsx
+++ src/Input.test.tsx
@@ -1,6 +1,17 @@
-import { shallow } from "enzyme";
+import React from "react";
+import { shallow, ShallowWrapper } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import Input from "./Input";
+
+const mockSetCurrentGuess = jest.fn();
+
+// Mock out react
+jest.mock("react", () => ({
+  // add all the contents of react back to module
+  ...jest.requireActual("react"),
+  // mock the useState function for testing mock
+  useState: (initialState: object) => [initialState, mockSetCurrentGuess],
+}));
 
 const setup = () => {
   return shallow(<Input secretWord="hockey" />);
@@ -10,4 +21,31 @@ test("renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-input");
   expect(component.length).toBe(1);
+});
+
+describe("state controlled input field", () => {
+  let wrapper: ShallowWrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    wrapper = setup();
+  });
+
+  test("state updates with value of input box upon change", () => {
+    const inputField = findByTestAttr(wrapper, "input-field");
+
+    const mockEvent = { target: { value: "hockey" } };
+
+    inputField.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("hockey");
+  });
+
+  test("setCurrentGuess is called with an empty string", () => {
+    const submitButton = findByTestAttr(wrapper, "input-submit-button");
+
+    const mockEvent = { preventDefault() {} };
+    submitButton.simulate("click", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
 });
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface InputProps {
   secretWord: string;
 }
 
+const checkSecretWord = (guessWord: string, secretWord: string) => {
+  if (guessWord === secretWord) {
+    return true;
+  }
+  return false;
+};
+
 const Input = ({ secretWord }: InputProps) => {
-  return <div data-test="component-input" />;
+  const [currentGuess, setCurrentGuess] = useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-field"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="input-submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(event) => {
+            event.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
